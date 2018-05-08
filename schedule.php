b6729fa25b341d97677abb40bb05a9b84f6a8399
Mostafa Biomee <biomee1982@gmail.com>
2018-05-08 10:53:33 +0200

    jq code

---
+++ schedule.php
@@ -0,0 +1,23 @@
+<?php
+//$_GET['date'] date in 'YYYY-MM-DD' format
+
+$ret = [];
+$statuses = [
+    ['active' => 1, 'description' => 'Open'],
+    ['active' => 1, 'description' => 'Almost full'],
+    ['active' => 0, 'description' => 'Full'],
+    ['active' => 0, 'description' => 'Closed']
+  ];
+
+
+$timestamp = strtotime($_GET['date']);
+
+for ($a = 0; $a < 7; $a++) {
+    $ret[] = [
+        'date' => date('Y-m-d', $timestamp),
+        'status' => $statuses[rand(0, 3)]
+    ];
+    $timestamp += 24*60*60;
+}
+
+echo json_encode($ret);
